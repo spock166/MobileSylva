4c5323a06de10d8eba78df9c67b8a51b73e9427c
spock166 <21161498+spock166@users.noreply.github.com>
2023-04-24 21:30:50 -0500

    Add API calls and GUI

---
+++ sylva.py
@@ -0,0 +1,85 @@
+
+import time
+import os, sys
+from sylvaBrain import *
+import sqlite3
+import tkinter as tk
+from PIL import Image, ImageTk
+from sylvaBrain import *
+
+class SylvaGUI:
+    def __init__(self):
+        self.background_color = '#385569'
+        self.textbox_color = '#496b82'
+        self.sentiment = Sentiment.neutral
+
+        self.root = tk.Tk()
+        self.root.resizable(False,False)
+        self.root.title("Sylva")
+        self.root.config(background=self.background_color)
+
+        # Create avatar for Sylva
+        self.avatar = ImageTk.PhotoImage(Image.open(os.path.join(sys.path[0],'pfp','neutral.png')).resize((512,512)))
+        self.avatar_label = tk.Label(self.root,image=self.avatar, background=self.background_color)
+        self.avatar_label.pack(side='top')
+
+        self.status = tk.StringVar()
+        self.info = tk.Label(self.root, bg=self.textbox_color, height=20,width=65,textvariable=self.status,justify="left", anchor="nw",wraplength=500)
+        self.info.pack(side='top')
+
+    def run(self):
+        self.root.after(10000,self.think)          
+        self.root.mainloop()
+
+    def think(self):
+        self.findNextQuestion()
+        self.root.after(10000,self.think)
+
+    def updateSentiment(self, sentiment:str):
+        print(os.path.join(sys.path[0],'pfp',sentiment+'.png'))
+        self.avatar = ImageTk.PhotoImage(Image.open(os.path.join(sys.path[0],'pfp',sentiment+'.png')).resize((512,512)))
+        self.avatar_label.configure(image=self.avatar)
+
+    def updateStatus(self, answer):
+        self.status.set(answer)
+
+    def findNextQuestion(self):
+        try:
+            sqliteConnection = sqlite3.connect(os.path.join(sys.path[0],'instance','sylva.sqlite'))
+            cursor = sqliteConnection.cursor()
+
+            res = cursor.execute('SELECT id, content FROM query WHERE answered=0 ORDER BY created')
+            fetch = res.fetchone()
+
+            if(fetch):
+                [ans,sentiment] = self.processFetch(fetch)
+                
+                update = "UPDATE query SET answered=1, answer=? WHERE id=?"
+                res=cursor.execute(update,(ans,str(fetch[0]),))
+                sqliteConnection.commit()
+
+        except sqlite3.Error as error:
+            print("Error while connecting to sqlite", error)
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
+
+    def processFetch(self,fetch):
+        print(fetch[1])
+        ans = Sylva().respond(fetch[1])
+        sentiment = Sylva().sentimentAnalysis(ans)
+        self.updateSentiment(sentiment)
+        self.updateStatus(ans)
+        self.root.update()
+        print(sentiment)
+        Sylva().synthesize_text(ans)
+        return [ans,sentiment]
+
+
+    def exitSylva(self):
+        self.root.destroy()
+
+
+if __name__ == "__main__":
+    myGUI = SylvaGUI()
+    myGUI.run()
---
+++ sylvaBrain.py
@@ -0,0 +1,86 @@
+import os, sys
+import json
+import openai
+from enum import Enum
+from google.cloud import texttospeech
+from pygame import mixer as pym
+import random
+
+
+#Load all the APIs!
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']=os.path.join(sys.path[0],'GoogleTTS.json')
+f = open(os.path.join(sys.path[0],'api_data.json'))
+data = json.load(f)
+openai.api_key = data['openai_token']
+f.close()
+
+class Sentiment(Enum):
+    neutral = "neutral"
+    positive = "positive"
+    sad = "sad"
+    mad = "mad"
+    surprised = "surprised"
+    funny = "funny"
+
+
+class Sylva:
+    def __init__(self, model_engine="gpt-3.5-turbo"):
+        self.model_engine = model_engine
+
+    def respond(self, message):
+        prompt = f"User: {message}\nSylva the vtuber:"
+        response = openai.ChatCompletion.create(
+            model=self.model_engine,
+            messages=[
+                {"role":"user","content":prompt},
+                {"role":"system","content":"Your name is Sylva and you are a catgirl who attends high school in Tokyo.  You should act like a vtuber when you respond"}
+            ],
+        )
+        return response['choices'][0]['message']['content']
+    
+    def sentimentAnalysis(self, answer):
+
+        prompt = f"Please analyze the sentiment of the following text and provide a one word output selected from the following list: neutral, positive, sad, mad, surprised, funny.  Your answer be exactly one word long. \n\nText: {answer}"
+        completions  = openai.Completion.create(
+            engine='text-babbage-001',
+            prompt=prompt,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+
+        response=completions.choices[random.randint(0,1)].text.strip()
+
+        for sentiment in Sentiment:
+            if(response.find(sentiment.name)!= -1):
+                return sentiment.name
+        
+        return Sentiment.neutral.name
+    
+    def synthesize_text(self, message):
+        """Synthesizes speech from the input string of text."""
+
+        client = texttospeech.TextToSpeechClient()
+        input_text = texttospeech.SynthesisInput(text=message)
+        # Note: the voice can also be specified by name.
+        # Names of voices can be retrieved with client.list_voices().
+        voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        name="en-US-Standard-F",
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+        )
+        audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+        )
+        response = client.synthesize_speech(
+            request={"input": input_text, "voice": voice, "audio_config": audio_config}
+        )
+        filename="voice.mp3"
+        with open(filename, "wb") as out:
+            out.write(response.audio_content)
+        pym.init()
+        pym.music.load(filename)
+        pym.music.play()
+        while pym.music.get_busy()==True:
+            continue
+        pym.music.unload()
